d37d0e8cbad8729f688004d6000bc743117bf6df
Desirée Hilti <desi.02@outlook.com>
2021-04-12 08:26:58 +0200

    Enemy

--- src/main/java/ch/bbw/gameboy/Enemy.java
+++ src/main/java/ch/bbw/gameboy/Enemy.java
@@ -1,15 +1,22 @@
 package ch.bbw.gameboy;
 
-public class Enemy implements Drawable{
+public class Enemy implements Drawable {
 
     private final Sprite sprite;
+    private int x;
+    private int y;
+    private final int speed;
 
-    public Enemy (Sprite sprite) {
+    public Enemy(Sprite sprite, int x, int y, int speed) {
         this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
     }
 
     @Override
     public void draw() {
-        sprite.draw(0,0);
+        y -= speed;
+        sprite.draw(x, y);
     }
 }
--- src/main/java/ch/bbw/gameboy/GameLogic.java
+++ src/main/java/ch/bbw/gameboy/GameLogic.java
@@ -13,35 +13,39 @@ import java.util.List;
  */
 public class GameLogic implements ButtonController {
 
-	private final PixelGraphic graphic;
+    private final PixelGraphic graphic;
 
-	private final StarfieldExample starfield;
+    private final StarfieldExample starfield;
 
-	private Player player;
+    private Player player;
     private List<Drawable> objects = new ArrayList<>();
 
-	public GameLogic(PixelGraphic graphic) {
-		this.graphic = graphic;
-		starfield = new StarfieldExample(graphic);
+    public GameLogic(PixelGraphic graphic) {
+        this.graphic = graphic;
+        starfield = new StarfieldExample(graphic);
 
         player = new Player(new Sprite("img_1.png", graphic));
-
+        objects.add(new Enemy(new Sprite("enemy1.jpg", graphic), 30, 30, 15));
+        objects.add(new Enemy(new Sprite("enemy2.jpeg", graphic), 50, 30, 15));
+        objects.add(new Enemy(new Sprite("enemy3.jpg", graphic), 120, 30, 15));
+        objects.add(new Enemy(new Sprite("enemy4.jpg", graphic), 80, 30, 15));
+        objects.add(new Enemy(new Sprite("Endboss.jpg", graphic), 80, 40, 10));
         objects.add(new Projectile(new Sprite("projectile.png", graphic), 30, 100, 1));
     }
 
-	public static void main(String[] args) throws Throwable {
-		GameBbwoy.main(args); // just here for a convenient button
-	}
+    public static void main(String[] args) throws Throwable {
+        GameBbwoy.main(args); // just here for a convenient button
+    }
 
-	/**
-	 * used to redraw the display. Executed with (more or less) constant fames per second.
-	 * @see GameBbwoy#FPS
-	 */
-	public void tick() {
-		graphic.clear();
-		starfield.draw();
-		player.draw();
-
+    /**
+     * used to redraw the display. Executed with (more or less) constant fames per second.
+     *
+     * @see GameBbwoy#FPS
+     */
+    public void tick() {
+        graphic.clear();
+        starfield.draw();
+        player.draw();
 
         var toRemove = new ArrayList<Drawable>();
         for (Drawable object : objects) {
@@ -54,15 +58,15 @@ public class GameLogic implements ButtonController {
         for (Drawable drawable : toRemove) {
             objects.remove(drawable);
         }
-	}
+    }
 
-	@Override
-	public void onButtonPress(GameButton button) {
-		System.out.println("down: " + button);
-	}
+    @Override
+    public void onButtonPress(GameButton button) {
+        System.out.println("down: " + button);
+    }
 
-	@Override
-	public void onButtonRelease(GameButton button) {
-		System.out.println("up: " + button);
-	}
+    @Override
+    public void onButtonRelease(GameButton button) {
+        System.out.println("up: " + button);
+    }
 }
