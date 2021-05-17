f51da713e248e69a9c1122a5826345c7dbafc47f
Nilstrieb <nils.heydecker@gmx.ch>
2021-05-17 08:56:13 +0200

    test

---
+++ src/test/java/ch/bbw/gameboy/SpriteTest.java
@@ -0,0 +1,17 @@
+package ch.bbw.gameboy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpriteTest {
+
+    @Test
+    void spriteTest() {
+        //assertDoesNotThrow(() -> new Sprite("sprites\\enemy1", null));
+
+        assertDoesNotThrow(() -> {});
+        assertTrue(true);
+
+    }
+}
